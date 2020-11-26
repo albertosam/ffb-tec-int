2042a0d97bc7f4cf36fc0748b4ba66f41bfcefce
Alberto Silas <albertosilas@gmail.com>
2020-11-25 21:56:50 -0300

    arquivos .net core

---
+++ server/TiV2/TiV2/Infrastructure/Repositories/CertificadoRepository.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Trilhas.Domain;
+
+namespace Trilhas.Infrastructure.Repositories
+{
+    public class CertificadoRepository : ICertificadoRepository
+    {
+        private readonly TrilhaContext _context;
+
+        public CertificadoRepository(TrilhaContext trilhaContext)
+        {
+            _context = trilhaContext;
+        }
+
+        public async Task<Certificacao> AddAsync(Certificacao entity)
+        {
+            await _context.Certificacoes.AddAsync(entity);
+            await _context.SaveChangesAsync();
+
+            return entity;
+        }
+
+        public Task<List<Certificacao>> GetAllAsync()
+        {
+            return _context.Certificacoes.ToListAsync();
+        }
+
+        public async Task<Certificacao> GetAsync(Guid id)
+        {
+            return await _context.Certificacoes
+                                            .Where(x => x.Id == id)
+                                            .FirstOrDefaultAsync();
+        }
+
+        public Task<Certificacao> UpdateAsync(Certificacao entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ server/TiV2/TiV2/Model/TrilhaCreate.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Trilhas.Api.Model
+{
+    public class TrilhaCreate
+    {
+        public string Descricao { get; set; }
+    }
+}
